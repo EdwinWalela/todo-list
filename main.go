e1820df2520b58a76643a448fbfca7095a64cdc8
EdwinWalela <edwinwalela@gmail.com>
2022-01-31 11:35:56 +0300

    CRUD routes

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"crafted.api/routes"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	PORT, _ := strconv.Atoi(os.Getenv("PORT"))
+
+	URL := fmt.Sprintf("0.0.0.0:%d", PORT)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/todos", routes.CreateTodo).Methods("POST")
+	r.HandleFunc("/todos", routes.GetTodos).Methods("GET")
+	r.HandleFunc("/todos", routes.UpdateTodo).Methods("PUT")
+	r.HandleFunc("/todos", routes.DeleteTodo).Methods("DELETE")
+
+	srv := &http.Server{
+		Addr:         URL,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
+	}
+
+	log.Printf("Listening for requests on port :%d\n", PORT)
+	log.Fatal(srv.ListenAndServe())
+}
---
+++ routes/todo.go
@@ -0,0 +1,19 @@
+package routes
+
+import "net/http"
+
+func GetTodos(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+
+}
