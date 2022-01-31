e49dae1b50095a3bb419d03aaf980da94fd1a17e
EdwinWalela <edwinwalela@gmail.com>
2022-01-31 12:00:01 +0300

    create todo'

--- routes/todo.go
+++ routes/todo.go
@@ -1,9 +1,46 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type Todo struct {
+	Title      string `json:"title"`
+	Timestamp  int64  `json:"timestamp"`
+	IsComplete bool   `json:"isComplete"`
+}
+
+type TodosResponse struct {
+	Todos []Todo `json:"todos"`
+}
 
 func GetTodos(w http.ResponseWriter, r *http.Request) {
 
+	todos := []Todo{
+		{
+			Title:      "Wash clothes",
+			Timestamp:  time.Now().Unix(),
+			IsComplete: false,
+		},
+		{
+			Title:      "Read book",
+			Timestamp:  time.Now().Unix(),
+			IsComplete: true,
+		},
+		{
+			Title:      "Ride bike",
+			Timestamp:  time.Now().Unix(),
+			IsComplete: false,
+		},
+	}
+
+	res := &TodosResponse{
+		Todos: todos,
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
 
 func CreateTodo(w http.ResponseWriter, r *http.Request) {
